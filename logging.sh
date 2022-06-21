fcff5c51c38ac2d0876c5e5e521463937ed71a3d
Franz Kurt <franz.gastring@gmail.com>
2022-06-21 11:36:58 -0300

    Update logging.sh

--- logging.sh
+++ logging.sh
@@ -1,6 +1,7 @@
 cria regra de log
 
 iptables -A INPUT -s 192.0.2.0/24 -p tcp -j LOG --dport 45 --log-prefix='[iptables] '
+sudo iptables -A INPUT -p tcp -j LOG --match multiport --dport 45,46 --log-prefix='[iptables] '
 
 /etc/rsyslog
 
